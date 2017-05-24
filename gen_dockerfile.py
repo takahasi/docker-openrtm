615ad966be412ed7747d7a37502c46de881f4b7e
takahasi <takahasi@sample.com>
2017-05-24 15:19:15 +0900

    all: replace dockerfile gnerated by script

---
+++ gen_dockerfile.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" script for generate dockerfile templates
+
+This is xxx
+"""
+
+import sys
+import os
+
+dockerfile_template = '''
+FROM @IMAGE_NAME@
+MAINTAINER @AUTHOR@
+
+ENV dist=@DISTRIBUTION@
+ENV rtmver=@RTM_VERSION@
+RUN set -x && \\
+    DEBIAN_FRONTEND=noninteractive apt-get -qq update && \\
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes curl bc && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_${rtmver}/OpenRTM-aist/build/pkg_install_${dist}.sh && \\
+    chmod a+x pkg_install_${dist}.sh && \\
+    ./pkg_install_${dist}.sh -s
+
+ENTRYPOINT ["/bin/bash", "-c"]
+EXPOSE 80
+'''
+
+class DockerImage:
+    def __init__(self, d, dv, i, a, rvmj, rvmn, rvrv):
+        self._dist = d
+        self._dist_version = dv
+        self._image = i
+        self._arch = a
+        self._rtm_version_major = rvmj
+        self._rtm_version_minor = rvmn
+        self._rtm_version_revision = rvrv
+
+    def create(self):
+        dist = self._dist + self._dist_version + self._arch
+        top_path = dist + '-openrtm' + self._rtm_version_major + self._rtm_version_minor + self._rtm_version_revision
+        if not os.path.exists(top_path):
+            os.makedirs(top_path)
+        f = open(top_path + '/Dockerfile', 'w')
+        print f
+        m = dockerfile_template
+        m = m.replace('@AUTHOR@', 'takahasi')
+        m = m.replace('@IMAGE_NAME@', self._image)
+        m = m.replace('@DISTRIBUTION@', self._dist)
+        m = m.replace('@RTM_VERSION@', self._rtm_version_major + '_' + self._rtm_version_minor + '_' + self._rtm_version_revision)
+        f.write(m)
+        f.close()
+
+
+DockerImage('ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '2').create()
+DockerImage('ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '1').create()
+DockerImage('ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '2').create()
+DockerImage('ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '1').create()
+DockerImage('ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '2').create()
+DockerImage('ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '1').create()
+DockerImage('ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '2').create()
+DockerImage('ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '1').create()
