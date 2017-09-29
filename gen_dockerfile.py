57de561a7b13af5a8d9053f81ce15af9a38e1bc7
takahasi <takahasi@example.com>
2017-09-29 15:41:10 +0900

    all: update for openrtm-1.2.0 beta release

--- gen_dockerfile.py
+++ gen_dockerfile.py
@@ -24,7 +24,65 @@ ENTRYPOINT ["/bin/bash", "-c"]
 EXPOSE 80
 '''
 
-dockerfile_template_all_desktop = '''
+dockerfile_template_12x_all_desktop = '''
+COPY pkg_install.sh /
+RUN set -x && \\
+    apt-get update -qq && \\
+    apt-get install -y bc iputils-ping net-tools && \\
+    apt-get install -y lxde xrdp && \\
+    chmod a+x ./pkg_install.sh && \\
+    ./pkg_install.sh -l all -c --yes && \\
+    apt-get autoclean -y && \\
+    apt-get autoremove -y && \\
+    RUN passwd -d root
+'''
+
+dockerfile_template_12x_all = '''
+COPY pkg_install.sh /
+RUN set -x && \\
+    apt-get update -qq && \\
+    apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && \\
+    ./pkg_install.sh -l all -c --yes && \\
+    apt-get autoclean -y && \\
+    apt-get autoremove -y
+'''
+
+dockerfile_template_12x_cxx = '''
+COPY pkg_install.sh /
+RUN set -x && \\
+    apt-get update -qq && \\
+    apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && \\
+    ./pkg_install.sh -l c++ -r --yes && \\
+    apt-get autoclean -y && \\
+    apt-get autoremove -y
+'''
+
+dockerfile_template_12x_python = '''
+COPY pkg_install.sh /
+RUN set -x && \\
+    apt-get update -qq && \\
+    apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && \\
+    ./pkg_install.sh -l python -r --yes && \\
+    apt-get autoclean -y && \\
+    apt-get autoremove -y
+'''
+
+dockerfile_template_12x_java = '''
+COPY pkg_install.sh /
+RUN set -x && \\
+    apt-get update -qq && \\
+    apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && \\
+    ./pkg_install.sh -l java -r --yes && \\
+    apt-get autoclean -y && \\
+    apt-get autoremove -y
+'''
+
+
+dockerfile_template_11x_all_desktop = '''
 RUN set -x && \\
     apt-get -qq update && \\
     apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
@@ -50,7 +108,7 @@ RUN set -x && \\
     RUN passwd -d root
 '''
 
-dockerfile_template_all = '''
+dockerfile_template_11x_all = '''
 RUN set -x && \\
     apt-get -qq update && \\
     apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
@@ -74,7 +132,7 @@ RUN set -x && \\
     apt-get autoremove -y
 '''
 
-dockerfile_template_cxx = '''
+dockerfile_template_11x_cxx = '''
 RUN set -x && \\
     apt-get -qq update && \\
     apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
@@ -85,7 +143,7 @@ RUN set -x && \\
     apt-get autoremove -y
 '''
 
-dockerfile_template_python = '''
+dockerfile_template_11x_python = '''
 RUN set -x && \\
     apt-get -qq update && \\
     apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
@@ -98,15 +156,16 @@ RUN set -x && \\
 '''
 
 class DockerImage:
-    def __init__(self, t, d, dv, i, a, rvmj, rvmn, rvrv):
-        self._dist = d
-        self._dist_version = dv
-        self._image = i
-        self._arch = a
+    def __init__(self, t, i, a, rv):
         self._type = t
-        self._rtm_version_major = rvmj
-        self._rtm_version_minor = rvmn
-        self._rtm_version_revision = rvrv
+        self._image = i
+        self._dist = self._image.split(":")[0]
+        self._dist_version = self._image.split(":")[1].replace(".", "")
+        self._arch = a
+        self._rtm_version = rv
+        self._rtm_version_major = self._rtm_version.split(".")[0]
+        self._rtm_version_minor = self._rtm_version.split(".")[1]
+        self._rtm_version_revision = self._rtm_version.split(".")[2]
 
     def create(self):
         dist = self._dist + self._dist_version + self._arch
@@ -114,24 +173,43 @@ class DockerImage:
             suffix = '-openrtm-cxx-'
         elif self._type == 'python':
             suffix = '-openrtm-python-'
+        elif self._type == 'java':
+            suffix = '-openrtm-java-'
         elif self._type == 'all':
             suffix = '-openrtm'
-        else:
+        else: # all+desktop
             suffix = '-openrtm-desktop'
         top_path = dist + suffix + self._rtm_version_major + self._rtm_version_minor + self._rtm_version_revision
         if not os.path.exists(top_path):
             os.makedirs(top_path)
         f = open(top_path + '/Dockerfile', 'w')
         print(f)
+
         m = dockerfile_template_common_header
-        if self._type == 'cxx':
-            m += dockerfile_template_cxx
-        elif self._type == 'python':
-            m += dockerfile_template_python
-        elif self._type == 'all':
-            m += dockerfile_template_all
+        if self._rtm_version == "1.2.0":
+            if self._type == 'cxx':
+                m += dockerfile_template_12x_cxx
+            elif self._type == 'python':
+                m += dockerfile_template_12x_python
+            elif self._type == 'java':
+                m += dockerfile_template_12x_java
+            elif self._type == 'all':
+                m += dockerfile_template_12x_all
+            else: # all+desktop
+                m += dockerfile_template_12x_all_desktop
+
         else:
-            m += dockerfile_template_all_desktop
+            if self._type == 'cxx':
+                m += dockerfile_template_11x_cxx
+            elif self._type == 'python':
+                m += dockerfile_template_11x_python
+            elif self._type == 'all':
+                m += dockerfile_template_11x_all
+            elif self._type == 'java':
+                m += dockerfile_template_11x_all
+            else: # all+desktop
+                m += dockerfile_template_11x_all_desktop
+
         m += dockerfile_template_common_footer
         m = m.replace('@AUTHOR@', 'takahasi <3263ta@gmail.com>')
         m = m.replace('@IMAGE_NAME@', self._image)
@@ -143,27 +221,32 @@ class DockerImage:
 
 if __name__=='__main__':
 
-    DockerImage('all_desktop', 'ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '2').create()
-    DockerImage('all_desktop', 'ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '2').create()
-    DockerImage('all_desktop', 'ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '2').create()
-    DockerImage('all_desktop', 'ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '2').create()
-    DockerImage('all_desktop', 'ubuntu', '1204', 'ubuntu:12.04', 'x64', '1', '1', '2').create()
-    DockerImage('all', 'ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '2').create()
-    DockerImage('all', 'ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '2').create()
-    DockerImage('all', 'ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '2').create()
-    DockerImage('all', 'ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '2').create()
-    DockerImage('all', 'ubuntu', '1204', 'ubuntu:12.04', 'x64', '1', '1', '2').create()
-    DockerImage('cxx', 'ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '2').create()
-    DockerImage('cxx', 'ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '2').create()
-    DockerImage('cxx', 'ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '2').create()
-    DockerImage('cxx', 'ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '2').create()
-    DockerImage('cxx', 'ubuntu', '1204', 'ubuntu:12.04', 'x64', '1', '1', '2').create()
-    DockerImage('python', 'ubuntu', '1704', 'ubuntu:17.04', 'x64', '1', '1', '2').create()
-    DockerImage('python', 'ubuntu', '1610', 'ubuntu:16.10', 'x64', '1', '1', '2').create()
-    DockerImage('python', 'ubuntu', '1604', 'ubuntu:16.04', 'x64', '1', '1', '2').create()
-    DockerImage('python', 'ubuntu', '1404', 'ubuntu:14.04', 'x64', '1', '1', '2').create()
-    DockerImage('python', 'ubuntu', '1204', 'ubuntu:12.04', 'x64', '1', '1', '2').create()
-    #DockerImage('debian', '8', 'debian:8', 'x64', '1', '1', '2').create()
-    #DockerImage('debian', '9', 'debian:9', 'x64', '1', '1', '2').create()
-    #DockerImage('fedora', '24', 'debian:24', 'x64', '1', '1', '2').create()
-    #DockerImage('fedora', '25', 'fedora:25', 'x64', '1', '1', '2').create()
+    version = [
+        "1.1.2",
+        "1.2.0"
+    ]
+
+    dists = [
+        "ubuntu:17.04",
+        "ubuntu:16.10",
+        "ubuntu:16.04",
+        "ubuntu:14.04",
+        "ubuntu:12.04",
+        # "debian:8",
+        # "debian:9",
+        # "fedora:24",
+        # "fedora:25",
+    ]
+
+    types = [
+        "all_desktop",
+        "all",
+        "cxx",
+        "python",
+        "java"
+    ]
+
+    for v in version:
+        for d in dists:
+            for t in types:
+                DockerImage(t, d, 'x64', v).create()
