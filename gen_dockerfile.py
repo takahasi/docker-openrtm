ea70af7661e1a42c77f411974d9e426e4e4fb62b
takahasi <takahasi@example.com>
2017-10-02 15:18:18 +0900

    all: rfactoring generate script

--- gen_dockerfile.py
+++ gen_dockerfile.py
@@ -9,17 +9,18 @@ This is generating script for OpenRTM on Docker
 import os
 
 dockerfile_template_common_header = '''
-FROM @IMAGE_NAME@
-LABEL maintainer="@AUTHOR@" \\
-      description="OpenRTM on Docker: OpenRTM-aist on Docker images."
+FROM {image}
+LABEL maintainer="{maintainer}" \\
+      description="OpenRTM on Docker: OpenRTM-aist on Docker images." \\
+      build_type="{buildtype}"
 
-ENV dist @DISTRIBUTION@
-ENV rtmver @RTM_VERSION@
 ENV DEBIAN_FRONTEND noninteractive
 
+RUN set -x && apt-get update -qq
 '''
 
 dockerfile_template_common_footer = '''
+RUN apt-get autoclean -y && apt-get autoremove -y
 
 CMD ["/etc/init.d/xvnc", "start"]
 ENTRYPOINT ["/bin/bash", "-c"]
@@ -28,82 +29,53 @@ EXPOSE 80
 
 dockerfile_template_12x_all_desktop = '''
 COPY pkg_install.sh /
-RUN set -x && \\
-    apt-get update -qq && \\
-    apt-get install -y bc iputils-ping net-tools && \\
+RUN apt-get install -y bc iputils-ping net-tools && \\
     apt-get install -y lxde xrdp && \\
-    chmod a+x ./pkg_install.sh && \\
-    sync && \\
+    chmod a+x ./pkg_install.sh && sync && \\
     ./pkg_install.sh -l all -c --yes && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y && \\
     yes root | passwd root
 '''
 
 dockerfile_template_12x_all = '''
 COPY pkg_install.sh /
-RUN set -x && \\
-    apt-get update -qq && \\
-    apt-get install -y bc iputils-ping net-tools && \\
-    chmod a+x ./pkg_install.sh && \\
-    sync && \\
-    ./pkg_install.sh -l all -c --yes && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+RUN apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && sync && \\
+    ./pkg_install.sh -l all -c --yes
 '''
 
 dockerfile_template_12x_cxx = '''
 COPY pkg_install.sh /
-RUN set -x && \\
-    apt-get update -qq && \\
-    apt-get install -y bc iputils-ping net-tools && \\
-    chmod a+x ./pkg_install.sh && \\
-    sync && \\
-    ./pkg_install.sh -l c++ -r --yes && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+RUN apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && sync && \\
+    ./pkg_install.sh -l c++ -r --yes
 '''
 
 dockerfile_template_12x_python = '''
 COPY pkg_install.sh /
-RUN set -x && \\
-    apt-get update -qq && \\
-    apt-get install -y bc iputils-ping net-tools && \\
-    chmod a+x ./pkg_install.sh && \\
-    sync && \\
-    ./pkg_install.sh -l python -r --yes && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+RUN apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && sync && \\
+    ./pkg_install.sh -l python -r --yes
 '''
 
 dockerfile_template_12x_java = '''
 COPY pkg_install.sh /
-RUN set -x && \\
-    apt-get update -qq && \\
-    apt-get install -y bc iputils-ping net-tools && \\
-    chmod a+x ./pkg_install.sh && \\
-    sync && \\
-    ./pkg_install.sh -l java -r --yes && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+RUN apt-get install -y bc iputils-ping net-tools && \\
+    chmod a+x ./pkg_install.sh && sync && \\
+    ./pkg_install.sh -l java -r --yes
 '''
 
 
 dockerfile_template_11x_all_desktop = '''
-RUN set -x && \\
-    apt-get -qq update && \\
-    apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
+RUN apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
     apt-get install -y python-pip && \\
     apt-get install -y default-jre && \\
     apt-get install -y lxde xrdp && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_${rtmver}/OpenRTM-aist/build/pkg_install_${dist}.sh && \\
-    chmod a+x pkg_install_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_${dist}.sh -c && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_${rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_${dist}.sh && \\
-    chmod a+x pkg_install_python_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_python_${dist}.sh -y && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_{rtmver}/OpenRTM-aist/build/pkg_install_{dist}.sh && \\
+    chmod a+x pkg_install_{dist}.sh && sync && \\
+    ./pkg_install_{dist}.sh -c && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_{rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_{dist}.sh && \\
+    chmod a+x pkg_install_python_{dist}.sh && sync && \\
+    ./pkg_install_python_{dist}.sh -y && \\
     pip install -U pip && \\
     pip install rtshell && \\
     mkdir -p /etc/bash_completion.d/ && \\
@@ -112,25 +84,19 @@ RUN set -x && \\
     tar xzf eclipse442-openrtp112v20160526-linux-gtk-x86_64.tar.gz && \\
     rm -rf eclipse442-openrtp112v20160526-linux-gtk-x86_64.tar.gz && \\
     ln -s /eclipse/openrtp /usr/bin/ && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y && \\
     yes root | passwd root
 '''
 
 dockerfile_template_11x_all = '''
-RUN set -x && \\
-    apt-get -qq update && \\
-    apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
+RUN apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
     apt-get install -y python-pip && \\
     apt-get install -y default-jre && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_${rtmver}/OpenRTM-aist/build/pkg_install_${dist}.sh && \\
-    chmod a+x pkg_install_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_${dist}.sh -c && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_${rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_${dist}.sh && \\
-    chmod a+x pkg_install_python_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_python_${dist}.sh -y && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_{rtmver}/OpenRTM-aist/build/pkg_install_{dist}.sh && \\
+    chmod a+x pkg_install_{dist}.sh && sync && \\
+    ./pkg_install_{dist}.sh -c && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_{rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_{dist}.sh && \\
+    chmod a+x pkg_install_python_{dist}.sh && sync && \\
+    ./pkg_install_python_{dist}.sh -y && \\
     pip install -U pip && \\
     pip install rtshell && \\
     mkdir -p /etc/bash_completion.d/ && \\
@@ -138,50 +104,37 @@ RUN set -x && \\
     curl -O http://openrtm.org/pub/openrtp/packages/1.1.2.v20160526/eclipse442-openrtp112v20160526-linux-gtk-x86_64.tar.gz && \\
     tar xzf eclipse442-openrtp112v20160526-linux-gtk-x86_64.tar.gz && \\
     rm -rf eclipse442-openrtp112v20160526-linux-gtk-x86_64.tar.gz && \\
-    ln -s /eclipse/openrtp /usr/bin/ && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+    ln -s /eclipse/openrtp /usr/bin/
 '''
 
 dockerfile_template_11x_cxx = '''
-RUN set -x && \\
-    apt-get -qq update && \\
-    apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_${rtmver}/OpenRTM-aist/build/pkg_install_${dist}.sh && \\
-    chmod a+x pkg_install_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_${dist}.sh -c && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+RUN apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
+    curl -O http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_{rtmver}/OpenRTM-aist/build/pkg_install_{dist}.sh && \\
+    chmod a+x pkg_install_{dist}.sh && sync && \\
+    ./pkg_install_{dist}.sh -c
 '''
 
 dockerfile_template_11x_python = '''
-RUN set -x && \\
-    apt-get -qq update && \\
-    apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
+RUN apt-get install -y curl bc bsdmainutils apt-utils aptitude iputils-ping net-tools && \\
     apt-get install -y python-pip && \\
-    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_${rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_${dist}.sh && \\
-    chmod a+x pkg_install_python_${dist}.sh && \\
-    sync && \\
-    ./pkg_install_python_${dist}.sh -y && \\
-    apt-get autoclean -y && \\
-    apt-get autoremove -y
+    curl -O http://svn.openrtm.org/OpenRTM-aist-Python/tags/RELEASE_{rtmver}/OpenRTM-aist-Python/installer/install_scripts/pkg_install_python_{dist}.sh && \\
+    chmod a+x pkg_install_python_{dist}.sh && sync && \\
+    ./pkg_install_python_{dist}.sh -y
 '''
 
 class DockerImage:
     def __init__(self, t, i, a, rv):
         self._type = t
         self._image = i
-        self._dist = self._image.split(":")[0]
-        self._dist_version = self._image.split(":")[1].replace(".", "")
-        self._arch = a
-        self._rtm_version = rv
-        self._rtm_version_major = self._rtm_version.split(".")[0]
-        self._rtm_version_minor = self._rtm_version.split(".")[1]
-        self._rtm_version_revision = self._rtm_version.split(".")[2]
+        self._dist = i.split(":")[0]
+        self._rtm_ver_major = rv.split(".")[0]
+        self._rtm_ver_minor = rv.split(".")[1]
+        self._rtm_ver_rev = rv.split(".")[2]
 
-    def create(self):
-        dist = self._dist + self._dist_version + self._arch
+        dist_ver = i.split(":")[1].replace(".", "")
+        arch = a
+
+        dist = self._dist + dist_ver + arch
         if self._type == 'cxx':
             suffix = '-openrtm-cxx-'
         elif self._type == 'python':
@@ -192,43 +145,66 @@ class DockerImage:
             suffix = '-openrtm'
         else: # all+desktop
             suffix = '-openrtm-desktop'
-        top_path = dist + suffix + self._rtm_version_major + self._rtm_version_minor + self._rtm_version_revision
-        if not os.path.exists(top_path):
-            os.makedirs(top_path)
-        f = open(top_path + '/Dockerfile', 'w')
-        print(f)
+        self._top_path = dist + suffix + self._rtm_ver_major + self._rtm_ver_minor + self._rtm_ver_rev
 
+    def dockerfile_11x(self):
         m = dockerfile_template_common_header
-        if self._rtm_version == "1.2.0":
-            if self._type == 'cxx':
-                m += dockerfile_template_12x_cxx
-            elif self._type == 'python':
-                m += dockerfile_template_12x_python
-            elif self._type == 'java':
-                m += dockerfile_template_12x_java
-            elif self._type == 'all':
-                m += dockerfile_template_12x_all
-            else: # all+desktop
-                m += dockerfile_template_12x_all_desktop
-
-        else:
-            if self._type == 'cxx':
-                m += dockerfile_template_11x_cxx
-            elif self._type == 'python':
-                m += dockerfile_template_11x_python
-            elif self._type == 'all':
-                m += dockerfile_template_11x_all
-            elif self._type == 'java':
-                m += dockerfile_template_11x_all
-            else: # all+desktop
-                m += dockerfile_template_11x_all_desktop
+        if self._type == 'cxx':
+            m += dockerfile_template_11x_cxx
+        elif self._type == 'python':
+            m += dockerfile_template_11x_python
+        elif self._type == 'java':
+            m += dockerfile_template_11x_all
+        elif self._type == 'all':
+            m += dockerfile_template_11x_all
+        else: # all+desktop
+            m += dockerfile_template_11x_all_desktop
 
         m += dockerfile_template_common_footer
-        m = m.replace('@AUTHOR@', 'takahasi <3263ta@gmail.com>')
-        m = m.replace('@IMAGE_NAME@', self._image)
-        m = m.replace('@DISTRIBUTION@', self._dist)
-        m = m.replace('@RTM_VERSION@', self._rtm_version_major + '_' + self._rtm_version_minor + '_' + self._rtm_version_revision)
-        f.write(m)
+        return m
+
+    def dockerfile_12x(self):
+        m = dockerfile_template_common_header
+        if self._type == 'cxx':
+            m += dockerfile_template_12x_cxx
+        elif self._type == 'python':
+            m += dockerfile_template_12x_python
+        elif self._type == 'java':
+            m += dockerfile_template_12x_java
+        elif self._type == 'all':
+            m += dockerfile_template_12x_all
+        else: # all+desktop
+            m += dockerfile_template_12x_all_desktop
+
+        m += dockerfile_template_common_footer
+        return m
+
+    def create(self):
+        if not os.path.exists(self._top_path):
+            os.makedirs(self._top_path)
+
+        f = open(self._top_path + '/Dockerfile', 'w')
+        print(f)
+
+        if self._rtm_ver_major == "1":
+            m = ""
+            if self._rtm_ver_minor == "2":
+                m = self.dockerfile_12x()
+            elif self._rtm_ver_minor == "1":
+                m = self.dockerfile_11x()
+            else:
+                print("Unknown version!!")
+                return
+
+            m = m.format(maintainer="takahasi <3263ta@gmail.com>",
+                         image=self._image,
+                         dist=self._dist,
+                         buildtype=self._dist + '-' + self._type,
+                         rtmver=self._rtm_ver_major + '_' + self._rtm_ver_minor + '_' + self._rtm_ver_rev)
+            f.write(m)
+        else:
+            print("Unknown version!!")
+
         f.close()
 
 
